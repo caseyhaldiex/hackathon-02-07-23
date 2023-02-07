7798f56d2b07b2cf9148a5a841e96d2221caffda
Casey Hald <casey.hald@iex.xyz>
2023-02-07 15:03:55 -0500

    loom

--- src/components/Loom.tsx
+++ src/components/Loom.tsx
@@ -1,9 +1,13 @@
 const Loom = () => {
   return (
-    <div>
-      Loom goes here
+    <div className="h-full w-full p-10">
+      <iframe
+        title="loom"
+        className="w-full h-full"
+        src="https://www.loom.com/embed/8c001dc1231c46f4b582b76807ad6a17"
+      ></iframe>
     </div>
-  )
+  );
 };
 
 export default Loom;
